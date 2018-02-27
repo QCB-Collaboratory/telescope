7b9586e6eda485cd0e76170b1c1da78d33e73b6a
Thiago Mosqueiro <thiago.mosqueiro@gmail.com>
2018-02-26 17:29:38 -0800

    Updated test script and doc based on Hackathon`s discussions

--- test/Readme.md
+++ test/Readme.md
@@ -1,6 +1,23 @@
-Scp all files to a folder called telescope_test
+# Telescope test
 
-Run on hoffman2:
+This is an example to test if telescope is able to find your jobs on the remote server. The script ```generate_test.py``` performs repeated diagonalizations and writes the eigenvalues on a output file.
+
+## Instructions to run this test
+
+For simplicity, we will assume we are working on the server [hoffman2.idre.ucla.edu](https://idre.ucla.edu/hoffman2). Start by cloning this repository:
+```shell
+git clone https://github.com/QCB-Collaboratory/telescope
+cd telescope
 ```
-qsub -cwd -V -N tlscpTest -l h_data=1G,time=6:10:00 runTlscpTest.sh
+Use scp to copy the test script to your user account:
+```shell
+scp telescope/test/* hoffman2.idre.ucla.edu:~
+```
+This will copy these files on your home directory. You can change the location of those files, but for simplicity let's assume they remain on your home directory. Then, log in to your account on hoffman2 and run the following qsub command:
+```
+qsub -cwd -V -N tlscpTest -l h_data=1G,time=0:30:00 runTlscpTest.sh
+```
+This should submit a job identified as ```tlscpTest```. It should be queued at first, but eventually it will get started. This job should take no longer than 30 minutes. To check the status of that job from the command line, run from hoffman2 the following command:
+```
+qstat | grep tlscpTest
 ```
--- test/generate_test.py
+++ test/generate_test.py
@@ -1,42 +1,58 @@
 import numpy as np
-import time
+import time, os
 
-finalBreak = 8*60*60
-numVars    = 500
-outputFile = open('output.dat', 'w', 0)
+## Initial configurations
+finalBreak     = 8*60*60
+numVars        = 500
+outputFilename = 'output.dat'
 
-startTime = time.time()
 
-iterID = 0
+## Actual script
+outputFile = open( outputFilename, 'w', 0)
+startTime  = time.time()
+iterID     = 0
 
+# Looping until time limit is reached
 while 1:
 
+    # First line for each iteration
     outputFile.write( 'Iteration ' + str(iterID).zfill(6) + '\n' )
 
+    # Checking if time limit was reached.
     currentDuration =  float( time.time() - startTime )
     if currentDuration >= finalBreak:
         break
 
+    # Generating some uncorrelated reandom variables
     X = np.random.random( ( numVars, 20000 ) )
 
+    # Creating artificial correlation among them
     numCorrelatedVars = np.random.randint( 100, high=100000, size=1 )
     i1 = np.random.randint( 0, high=numVars-1, size=numCorrelatedVars )
     i2 = np.random.randint( 0, high=numVars-1, size=numCorrelatedVars )
     for j in range(numCorrelatedVars):
         X[i1[j],:] = ( X[i1[j],:] + X[i2[j],:] ) / 2.
 
+    # Calculating their covariance matrix and evaluating
+    # their eigenvalues and eigenvectors
     covMat = np.cov(X)
     w, v   = np.linalg.eig( covMat )
 
+    # Writing the sorted eigenvalues
     outputFile.write( str( np.sort( np.abs(w) )[-1:-15:-1] ) )
 
+    # Separation between iterations
     outputFile.write('\n---\n')
 
+    # updating iteration variable
     iterID += 1
 
+    ## To avoid an excessively large output file,
+    ## let's limit it to the last 3000 lines every
+    ## thousand iterations.
     if iterID > 1000:
         outputFile.close()
-        os.system( "tail -n 3000 output.dat > output.dat" )
+        os.system("tail -n 3000 output.dat > output.dat")
         outputFile = open('output.dat', 'w', 0)
 
 
