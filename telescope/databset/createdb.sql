57b941eba27a2b2e76d2a6f77e7811769d0a63f7
Jaque J Brito <jaquejbrito@gmail.com>
2018-03-20 16:03:13 -0700

    updating db script

--- telescope/databset/createdb.sql
+++ telescope/databset/createdb.sql
@@ -1,24 +1,23 @@
-create table jobs2(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-user VARCHAR(30),
-jobname TEXT,                 -- Unique job id
+create table jobs(
+jobId INTEGER PRIMARY KEY,    -- Unique job id
+jobName TEXT,                 -- Name of the job
+user VARCHAR(30),             -- Username
+status INTEGER,               -- Current status of job (as of last time qstat was updated)
+path TEXT,                    -- Path to the script that is being run
 command TEXT,                 -- Command used to submit job
 sourceDirectory TEXT,         -- Directory from which the job was submitted
 outpath TEXT,                 -- Path and name for output file
-path TEXT,                    -- Path to the script that is being run
 memoryRequested TEXT,         -- Amount of memory requested
 parallel INTEGER,             -- Running in parallel (1) or not (0)
 cores INTEGER,                -- How many cores requested?
 timeAdded VARCHAR(30),        -- When was this entry added to the database?
-status INTEGER,               -- Current status of job (as of last time qstat was updated)
 runTime TEXT,                 -- Time job has been running on Hoffman
 timeRemaining TEXT,           -- Time remaining before job is killed by Hoffman
 currentMemory INTEGER,        -- Memory currently in use by job
 maximumMemory INTEGER,        -- Maximum memory used so far in job's history
-HoffmanJobID INTEGER,         -- Job number given by Hoffman
-HoffmanNode TEXT,             -- Node on which job was run
+clusterNode TEXT,             -- Node on which job was run
 finalRunTime TEXT,            -- For finished jobs, how long did they run?
 howEnded TEXT                 -- How did the job end? Completed, killed by user, killed by Hoffman, aborted?
 );
 
-insert into jobs2 (user,jobname, command,sourceDirectory , outpath, path, memoryRequested, parallel, cores, timeAdded, status, runTime, timeRemaining, currentMemory, maximumMemory, HoffmanJobID, HoffmanNode, finalRunTime, howEnded) values ('thmosque','test','ls -l ','/home/thmosque', '/home/thmosque', '/home/thmosque/out', '1GB', 1, 1, 'Fri Feb 23 16:08:42 PST 2018', 0, 0, '8:00:00', 0, 0, 988675, '78', 'NA', 'NA');
+--insert into jobs (jobId, jobName, user, status, path) values (1, 'test', 'username,'1', '/home/username');
